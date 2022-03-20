ca8e78420338f4e59d0e264888be9e3ecd3687e2
Deb-neal <dksaksen7@gmail.com>
2022-03-20 21:44:08 +0900

    make node.js server

--- devcat/server/app.js
+++ devcat/server/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("hello node");
+  res.status(201).send("Hello World");
 });
+
+app.get("/hello", function (req, res) {
+  res.send("hello world");
+});
+
+app.use(express.static("index.html"));
 
 app.listen(4000, () => console.log("4000번 포트에서 대기중"));
